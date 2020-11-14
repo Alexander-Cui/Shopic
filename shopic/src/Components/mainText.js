4c2c8f1f8315e1ddc9acb4d950582dfdcd3bcc45
janec24 <jane_c24@hotmail.com>
2020-11-14 16:27:42 -0500

    basic components for frontpage

---
+++ shopic/src/Components/mainText.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./mainText.css"
+
+function MainText(){
+    return(
+        <div>
+            <h1 className="line1"> Shop visually.</h1>
+            <h2 className="line2">upload your inspo pic here!</h2>
+        </div>
+    )
+}
+
+export default MainText
