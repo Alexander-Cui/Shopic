3c4a46e8b206ee4e63a3f2334c1cc4a1f989ffdd
janec24 <jane_c24@hotmail.com>
2020-11-15 00:23:24 -0500

    styled gallery page with photo placeholders

--- shopic/src/Components/GalleryPage.js
+++ shopic/src/Components/GalleryPage.js
@@ -1,9 +1,23 @@
-
+import "./GalleryPage.css"
+import Uploaded from "../images/homepage.jpg"
+import Icon from "../images/shopic_logo.png"
 
 const GalleryPage = ({ ...props }) => {
+const images = ['./images/homepage.jpg', 'path2', Uploaded, Uploaded, Uploaded]
+
     return (
         <div>
-            <h1>HEOOOO</h1>
+            <h2 className="title" > SHOPIC</h2>
+            <div className="iconContainer">
+            {images.map(path => {
+                return(
+           
+                    <img src={Uploaded} alt="hello" className="itemIcon"/>
+    
+                )
+            })}
+            </div>
+            <img src={Icon} className="icon"></img>
         </div>
     )
 }
